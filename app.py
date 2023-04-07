88a39020f648efa68b8e112ed7d7eaa48c7729bc
hoangngo <huy.hoang@conciergeu.com>
2023-04-08 02:19:25 +0700

    remove resources

--- app.py
+++ app.py
@@ -2,32 +2,13 @@ from flask import Flask, g, request, Response
 import json
 from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader, LLMPredictor, PromptHelper
 from dotenv import load_dotenv
-import qa
-import scrape
 
 import os
-import sentry_sdk
-from sentry_sdk.integrations.flask import FlaskIntegration
-import crawl
-import s3_ultilities
 
-bucket_name = 'conciergeu-ai-bot-stgv6'
-s3_ultilities.init_data(bucket_name, ['index', 'data_crawled'])
-
-sentry_sdk.init(
-    dsn=os.environ['SENTRY_DNS'],
-    integrations=[
-        FlaskIntegration(),
-    ],    
-    traces_sample_rate=1.0
-)
 
 load_dotenv()
 app = Flask(__name__)
 
-@app.route('/debug-sentry')
-def trigger_error():
-    division_by_zero = 1 / 0
 
 @app.route('/healthcheck')
 def healthcheck():
@@ -36,40 +17,8 @@ def healthcheck():
 @app.route('/message', methods=['POST'])
 def get_bot_message():
     query = request.form.get("query")
-    original_service_id = request.form.get("original_service_id")
     print(f"query: {query}")
-    index = GPTSimpleVectorIndex.load_from_disk(os.path.join('index',f'index_{original_service_id}.json'))
+    index = GPTSimpleVectorIndex.load_from_disk(os.path.join('index',f'index_crawled.json'))
     response = index.query(query)
     print(f"response: {response.get_formatted_sources()}")
     return {"message": json.dumps(response.response, indent=2, ensure_ascii=False)}
-
-@app.route('/index', methods=['POST'])
-def generate_index():
-    mode = request.form.get("mode")
-    if mode == "all":
-        original_service_id = "crawled"
-    else:
-        url = request.form.get("url")
-        original_service_id = request.form.get("original_service_id")
-        if not original_service_id:
-            return {"status": "Error: original_service_id is not found."}
-        elif not url:
-            return {"status": "Error: url is not found."}
-        else:
-            title, contents = scrape.get_text_from_url(url)
-            scrape.save_contents(title, contents, original_service_id)
-
-    qa.generate_index(f"data_{original_service_id}/", original_service_id)
-    s3_ultilities.upload_directory(bucket_name, 'index')
-    return {"status": "Success"}
-
-
-@app.route('/crawl', methods=['POST'])
-def start_crawl():
-    try:
-        crawl.start_crawl()
-        s3_ultilities.upload_directory(bucket_name, 'data_crawled')
-        return {"status": "Success"}
-    except Exception as e:
-        print(e)
-        return {"status": "Fail"}
